fd226a240918c28bd04346e988e2e0990e655647
Evellyn <flaviane.evellyn@alunoescolassociais.org.br>
2023-10-24 10:52:03 -0300

    alteração

--- cadastro.js
+++ cadastro.js
@@ -30,45 +30,19 @@ function Verifica(){
         
     }
 }
-'use strict'; //Modo restrito // https://viacep.com.br/
-
-//Função para limpar formulário
-//Arrow Function
-const limparFormulario = () => {
-    document.getElementById('rua').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('estado').value = '';
-}
-// Verifica se CEP é válido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-const cepValido = (cep) => cep.length == 8 && eNumero(cep);
-
-//Responsável pelo preenchimento do formulário 
-const preencherFormulario = (endereco) => {
-    document.getElementById('rua').value = endereco.logradouro;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endereco.localidade;
-    document.getElementById('estado').value = endereco.uf;
-}
-// Função para consumo de API da Via CEP
-const pesquisarCep = async() => {
-    limparFormulario();
-    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
-
-    if(cepValido(cep.value)){
-        const dados = await fetch(url); // esperar
-        const address = await dados.json(); // retorna dados no formato JSON
-
-        if(address.hasOwnProperty('erro')){
-            alert('CEP não encontrado');
-        }else{
-            preencherFormulario(address);
-        }
-    }else{
-        alert('CEP incorreto');
+function formatarCampo(campoTexto) {
+    if (campoTexto.value.length <= 11) {
+        campoTexto.value = mascaraCpf(campoTexto.value);
+    } else {
+        campoTexto.value = mascaraCnpj(campoTexto.value);
     }
-
 }
-// Adicionar um evento DOM no input do CEP
-document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+function retirarFormatacao(campoTexto) {
+    campoTexto.value = campoTexto.value.replace(/(\.|\/|\-)/g,"");
+}
+function mascaraCpf(valor) {
+    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"\$1.\$2.\$3\-\$4");
+}
+function mascaraCnpj(valor) {
+    return valor.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,"\$1.\$2.\$3\/\$4\-\$5");
+}
